e3de0731cda4f1ad651d70ee31aa833f7ff252ec
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-23 16:23:35 +0100

    shortened code with  bool array method

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -10,35 +10,39 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1,2 or 3: ");
             string userValue = Console.ReadLine();
 
-            if (userValue == "1")
-            {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
-            else if (userValue == "2")
-            {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
-     
-            }
-            else if (userValue == "3")
-            {
-                string message = "You won a new Cat!";
-                Console.WriteLine(message);
+            string message = "";
 
-            }
+            if (userValue == "1")
+                message = "You won a new car!";
+            else if (userValue == "2")
+                message = "You won a new boat!";
+            else if (userValue == "3")
+                message = "You won a new Cat!";
             else
             {
-                string message = "sorry, we didn't understand";
-                Console.WriteLine(message);
+                message = "sorry, we didn't understand";
+                //message = message + "You lose.";
+                message += "You lose.";
             }
 
+            Console.WriteLine(message);
             Console.ReadLine();
+            */
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1,2 or 3: ");
+            string userValue = Console.ReadLine();
 
+            string message = (userValue == "1") ? "boat":"strand of lint";
+
+            Console.Write("You won a ");
+            Console.Write(message);
+            Console.Write(".");
+            Console.ReadLine();
         }
     }
 }
